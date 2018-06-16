78fe8eb5b4247de511b916b9bb53755e84a91063
zaeval <h3065@naver.com>
2018-06-16 13:31:21 +0900

    Body load animation add

--- eval/static/script/index.js
+++ eval/static/script/index.js
@@ -4,20 +4,21 @@ $(document).ready(
     function () {
         $('<img/>').attr('src', 'static/ktis-background.png').on('load', function () {
             $(this).remove(); // prevent memory leaks as @benweet suggested
-            $('body').css('background-image', 'url(static/ktis-background.png)');
-            
-            var navMenus = $(".nav-wrapper").find('section');
-            console.log(navMenus);
-            for (var i = 0; i < navMenus.length; ++i) {
-                let second = delay * i / 1000;
-                $(navMenus[i]).css(
-                    {
-                        'animation-delay': second + 's',
-                        'display':'inline-block'
-                    });
-                $(navMenus[i]).addClass('fade-in animated');
+            $('body')
+                .css('background-image', 'url(static/ktis-background.png)');
+            $('body').fadeIn(1500, function () {
+                    var navMenus = $(".nav-wrapper").find('section');
+                    for (var i = 0; i < navMenus.length; ++i) {
+                        let second = delay * i / 1000;
+                        $(navMenus[i]).css({
+                            'animation-delay': second + 's',
+                            'display': 'inline-block'
+                        });
+                        $(navMenus[i]).addClass('fade-in animated');
 
-            }
+                    }
+                });
+
         });
     }
 );

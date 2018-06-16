d9c7ca8831e9abd325f61b1cc65e72b17451e991
zaeval <h3065@naver.com>
2018-06-16 13:16:33 +0900

    Img onload

--- eval/static/script/index.js
+++ eval/static/script/index.js
@@ -1,13 +1,23 @@
 let delay = 300;
 $(document).ready(
-    function(){
-        var navMenus = $(".nav-wrapper").find('section');
-        for(var i = 0; i<navMenus.length; ++i){
-            let second = delay * i / 1000;
-            $(navMenus[i]).css('animation-delay', second+'s');
+
+    function () {
+        $('<img/>').attr('src', 'static/ktis-background.png').on('load', function () {
+            $(this).remove(); // prevent memory leaks as @benweet suggested
+            $('body').css('background-image', 'url(static/ktis-background.png)');
             
-            $(navMenus[i]).addClass('fade-in animated');
-            
-        }
+            var navMenus = $(".nav-wrapper").find('section');
+            console.log(navMenus);
+            for (var i = 0; i < navMenus.length; ++i) {
+                let second = delay * i / 1000;
+                $(navMenus[i]).css(
+                    {
+                        'animation-delay': second + 's',
+                        'display':'inline-block'
+                    });
+                $(navMenus[i]).addClass('fade-in animated');
+
+            }
+        });
     }
 );
